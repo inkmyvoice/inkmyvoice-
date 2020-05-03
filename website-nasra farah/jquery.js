7eeedb9f8f4d8e77c1e297fee671e7c387bf02e6
inkmyvoice <64714334+inkmyvoice@users.noreply.github.com>
2020-05-02 17:32:38 -0700

    Add files via upload

---
+++ website-nasra farah/jquery.js
@@ -0,0 +1,21 @@
+
+function myChapter(){
+var i = document.getElementById("eachchapter");
+if (i.style.display==="block"){
+    i.style.display="none";
+}
+else{
+    i.style.display="block";
+}
+}
+
+    function showMore(){
+    var x = document.getElementById("showmore");
+    if (x.style.display ==="block"){
+    x.style.display ="none";
+    }
+    else{
+    x.style.display = "block";
+    }
+}
+  
